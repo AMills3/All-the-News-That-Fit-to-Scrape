08a5204b96d5ef4f55a96cf882d8061f180e9705
Adam Miller <miad0801@gmail.com>
2019-11-11 17:15:16 -0600

    Updated spelling

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 // Exporting the object containing the models
 
 module.exports = {
-    Article: require("./Article"),
-    Note: require("./Note")
+    Article: require("./article"),
+    Note: require("./note")
   };
