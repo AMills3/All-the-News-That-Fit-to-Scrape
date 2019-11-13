4438b7fe9b0a9e0395399853970495414fdc3264
Adam Miller <miad0801@gmail.com>
2019-11-12 19:06:00 -0600

    Updated

--- public/app.js
+++ public/app.js
@@ -2,14 +2,14 @@ $(document).on("click", "submit", function() {
     
     $.getJSON("/scrape", function(data) {
       for (var i = 0; i < data.length; i++) {
-        $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+        $("#theArticle").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
       }
     });
   });
 
 // On click event
 $(document).on("click", "p", function() {
-    $("#notes").empty();
+    $("#theNote").empty();
     let thisId = $(this).attr("data-id");
 
 // Making the Ajax call
@@ -21,10 +21,10 @@ $.ajax({
 // Adding note info
 .then(function(data) {
 console.log(data);
-    $("#notes").append("<h1>" + data.title + "</h1>");
-    $("#notes").append("<input id='titletext' name='title' >");
-    $("#notes").append("<textarea id='bodytext' name='body'></textarea>");
-    $("#notes").append("<button data-id='" + data._id + "' id='notesave'>Save Note</button>");
+    $("#theNote").append("<h1>" + data.title + "</h1>");
+    $("#theNote").append("<input id='titletext' name='title' >");
+    $("#theNote").append("<textarea id='bodytext' name='body'></textarea>");
+    $("#theNote").append("<button data-id='" + data._id + "' id='notesave'>Save Note</button>");
 
 // Adding more info to the note
 if (data.note) {
@@ -49,7 +49,7 @@ $.ajax({
     })
 // Emptying the note section
 .then(function(data) {
-    $("#notes").empty();
+    $("#theNote").empty();
     });
   
 // Entering data for the note
